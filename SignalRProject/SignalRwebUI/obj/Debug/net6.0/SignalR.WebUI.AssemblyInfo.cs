777d1f9f7e6da7b1b368682dc09689518b1e0959
Sedat Başkar <sedatbaskar@gmail.com>
2024-01-06 20:49:40 +0300

    ajax registration in menu action
    ajax registration in menu action

--- SignalRProject/SignalRwebUI/obj/Debug/net6.0/SignalR.WebUI.AssemblyInfo.cs
+++ SignalRProject/SignalRwebUI/obj/Debug/net6.0/SignalR.WebUI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e4f67b43260cd4cec756bb4551be1398cc1df06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b8522665d101af03ee82a893494680c8514067")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
